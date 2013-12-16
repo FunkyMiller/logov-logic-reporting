64921e124c863f044c3d13093d4b819884482cdd
unknown <funky@.civica.com.au>
2013-12-16 21:51:39 +1100

    Commit refolderisation and shell report.php

---
+++ report.php
@@ -0,0 +1,14 @@
+<HTML>
+<HEAD>
+	<meta name="description" content="LoGov Logic Reporting">
+	<meta name="keywords" content="Authority,Reporting,LoGov Logic">
+	<meta name="author" content="LoGov Logic">
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<script type="text/javascript" src="scripts\jquery-1.10.2.min.js"></script>
+	<script type="text/javascript" src="scripts\jquery.dataTables.min.js"></script>
+</HEAD
+<BODY>
+</BODY>
+<FOOT>
+</FOOT>
+</HTML>
