d9dc0db2130641735c839fd1bb45c34703066231
FunkyMiller <funkymiller@internode.on.net>
2013-12-17 14:59:06 +1100

    Initial commit of scripts

---
+++ logov-reporting.php
@@ -0,0 +1,28 @@
+<HTML>
+<HEAD>
+	<meta name="description" content="Free Web tutorials">
+	<meta name="keywords" content="HTML,CSS,XML,JavaScript">
+	<meta name="author" content="Hege Refsnes">
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<script type="text/javascript" src="scripts/jquery-1.10.2.min.js"></script>
+	<script type="text/javascript" src="scripts/jquery.dataTables.min.js"></script>
+	<script type="text/javascript" src="scripts/jquery.dataTables.columnFit.js"></script>
+	<script type="text/javascript" src="scripts/jquery.dataTables.rowGroup.js"></script>
+	<script type="text/javascript" src="scripts/jquery.dataTables.rowReorder.js"></script>
+	<script type="text/javascript" src="scripts/dataTables.scroller.min.js"></script>
+	<script type="text/javascript" src="scripts/KeyTable.min.js"></script>
+	<script type="text/javascript" src="scripts/TableTools.min.js"></script>
+	<script type="text/javascript" src="scripts/ZeroClipboard.min.js"></script>
+	<script type="text/javascript" src="scripts/FixedHeader.min.js"></script>
+	<script type="text/javascript" src="scripts/FixedColumns.min.js"></script>
+	<script type="text/javascript" src="scripts/AutoFill.min.js"></script>
+	<script type="text/javascript" src="scripts/ColReorder.min.js"></script>
+	<script type="text/javascript" src="scripts/ColumnFilterWidget.js"></script>
+	<script type="text/javascript" src="scripts/ColVis.min.js"></script>
+</head>
+</HEAD>
+<BODY>
+</BODY>
+<FOOT>
+</FOOT>
+</HTML>
